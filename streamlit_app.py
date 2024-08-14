bfba92c21d68b2402ec07d6730e1b06df1880883
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-14 11:59:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ fig.add_trace(
 )
 
 
-fig.update_traces(marker=dict(size=8,
+fig.update_traces(marker=dict(size=6,
                               line=dict(width=0.3, color='black')),
                   selector=dict(mode='markers'))
 fig.update_layout(
