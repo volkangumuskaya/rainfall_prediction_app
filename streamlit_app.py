1d4252126143f755987f7fca4c052993922ed80f
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-14 15:34:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import math
 from pathlib import Path
 
+
+
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='volkan-ai',
@@ -62,7 +64,7 @@ if not len(stations):
 selected_stations = st.multiselect(
     'Which station would you like to view?',
     stations,
-    ["Eindhoven"])
+    ["Eindhoven","Rotterdam"])
 
 ''
 
