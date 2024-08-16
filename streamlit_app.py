3e2b0f0848568f4125a026397c160fe6a2adb430
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-16 22:08:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ df = get_df(filename)
 st.image('images/el-chalten.jpg','El Chalten, Patagonia')
 st.sidebar.title("")
 with st.sidebar:
-    st.image('images/zibzib-warhol.png','Zibzib')
+    st.image('images/zibzib-warhol.jpg','Zibzib')
 '''
 # :rain_cloud: Daily rain predictions for the cities in Netherlands
 
