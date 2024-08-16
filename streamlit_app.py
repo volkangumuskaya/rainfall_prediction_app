78a35f48859e2915fbc9f673ec1e29c12f9a9bb5
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-16 15:40:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,7 +213,7 @@ for i in range(0, len(df)):
         with col:
             st.metric(
                 label=df.columns[j],
-                value=df.iloc[i][j]
+                value=str(int(df.iloc[i][j]*100)/100)
             )
 
 
