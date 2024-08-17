48715340e689cbe55ca45c805241ea85e5143368
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-17 12:18:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import math
 from pathlib import Path
 
-
+st.cache_resource.clear()
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='volkan-ai',
