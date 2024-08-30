4b75df73ea516bb74b6495839c35ec68fd4666e6
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-30 15:47:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -291,118 +291,118 @@ figure=plot_max_min_temps(selected_stations=selected_station_t,
                          )
 st.plotly_chart(figure)
 
-# #Predictions part
-# filename='files/daily_prediction.csv'
-# df = get_df(filename)
-# if(len(df)>1):
+#Predictions part
+filename='files/daily_prediction.csv'
+df = get_df(filename)
+if(len(df)>1):
         
-#     ''
-#     st.header('Rainfall amounts', divider=True)
-#     ''
+    ''
+    st.header('Rainfall amounts', divider=True)
+    ''
     
     
-#     df=df.fillna(0)
-#     df = df.replace('',0)
-#     min_value = df['date'].min()
-#     max_value = df['date'].max()
-#     print("min",min_value,'max',max_value)
+    df=df.fillna(0)
+    df = df.replace('',0)
+    min_value = df['date'].min()
+    max_value = df['date'].max()
+    print("min",min_value,'max',max_value)
     
-#     from_year, to_year = st.select_slider(
-#         "Select a range of date",
-#         options=df.date.sort_values().unique(),
-#         value=(min_value, max_value),
-#     )
-#     ''
-#     stations = df['station_name'].sort_values().unique()
+    from_year, to_year = st.select_slider(
+        "Select a range of date",
+        options=df.date.sort_values().unique(),
+        value=(min_value, max_value),
+    )
+    ''
+    stations = df['station_name'].sort_values().unique()
     
-#     if not len(stations):
-#         st.warning("Select at least one station")
+    if not len(stations):
+        st.warning("Select at least one station")
     
-#     selected_stations = st.multiselect(
-#         'Which station would you like to view?',
-#         stations,
-#         ["Eindhoven","Rotterdam"])
+    selected_stations = st.multiselect(
+        'Which station would you like to view?',
+        stations,
+        ["Eindhoven","Rotterdam"])
     
-#     ''
+    ''
     
-#     # Filter the data
-#     filtered_df = df[
-#         (df['station_name'].isin(selected_stations))
-#         & (df['date'] <= to_year)
-#         & (from_year <= df['date'])
-#     ]
-    
-    
-#     ''
-#     filtered_df['date']=filtered_df['date'].astype('str')
-    
-#     ''
-#     st.bar_chart(
-#         filtered_df,
-#         x='date',
-#         y='next_day_rain_mm',
-#         color='station_name',
-#         y_label='Rainfall amount',
-#         stack=False
-#     )
-#     ''
-    
-#     selected_stat = st.selectbox(
-#         'Which station to be inspected in detail vis-a-vis predictions?',
-#         stations
-#     )
-    
-#     #plot detailed
-#     df = df[
-#         (df['station_name']==selected_stat)
-#         & (df['date'] <= to_year)
-#         & (from_year <= df['date'])
-#     ].copy()
-#     df['date']=df['date'].astype('str')
-#     df['error']=df['rain_amount_mm_prediction']-df['next_day_rain_mm']
-#     y_max=np.ceil(max(df.rain_amount_mm_prediction.max(),df.next_day_rain_mm.max())/20)*20
-#     y_min=-y_max
-    
-#     kwargs = {
-#         'cbar': False,
-#         'linewidths': 0.2,
-#         'linecolor': 'white',
-#         'annot': True}
-    
-#     #Start creating plotly plot
-#     fig = make_subplots(specs=[[{"secondary_y": True}]])
-#     fig.add_trace(
-#         go.Bar(x=df.date, y=df.error,
-#                # text=round(metrics.R2, 2),
-#                marker_color='dodgerblue', opacity=0.7,
-#                name="Error"),
-#         secondary_y=False
-#     )
-    
-#     fig.add_trace(
-#         go.Scatter(x=df.date, y=df.next_day_rain_mm,
-#                    mode='markers', name="Next day rain"),
-#         secondary_y=True
-#     )
-    
-#     fig.add_trace(
-#         go.Scatter(x=df.date, y=df.rain_amount_mm_prediction,
-#                    mode='markers', name="Rain prediction"),
-#         secondary_y=True
-#     )
+    # Filter the data
+    filtered_df = df[
+        (df['station_name'].isin(selected_stations))
+        & (df['date'] <= to_year)
+        & (from_year <= df['date'])
+    ]
     
     
-#     fig.update_traces(marker=dict(size=5.5,
-#                                   line=dict(width=0.3, color='black')),
-#                       selector=dict(mode='markers'))
-#     fig.update_layout(
-#         title="Detailed predictions, actuals and errors for selected station",
-#         xaxis_title="Date",
-#         yaxis_title="Rain amount",
-#         legend_title="Legend",
-#     )
-#     fig.update_layout(xaxis_type='category')
-#     fig.update_xaxes(tickangle=270)
-#     fig.update_yaxes(range=[y_min,y_max], secondary_y=False)
-#     fig.update_yaxes(range=[y_min,y_max],title_text="Rain amount", secondary_y=True)
-#     st.plotly_chart(fig)
+    ''
+    filtered_df['date']=filtered_df['date'].astype('str')
+    
+    ''
+    st.bar_chart(
+        filtered_df,
+        x='date',
+        y='next_day_rain_mm',
+        color='station_name',
+        y_label='Rainfall amount',
+        stack=False
+    )
+    ''
+    
+    selected_stat = st.selectbox(
+        'Which station to be inspected in detail vis-a-vis predictions?',
+        stations
+    )
+    
+    #plot detailed
+    df = df[
+        (df['station_name']==selected_stat)
+        & (df['date'] <= to_year)
+        & (from_year <= df['date'])
+    ].copy()
+    df['date']=df['date'].astype('str')
+    df['error']=df['rain_amount_mm_prediction']-df['next_day_rain_mm']
+    y_max=np.ceil(max(df.rain_amount_mm_prediction.max(),df.next_day_rain_mm.max())/20)*20
+    y_min=-y_max
+    
+    kwargs = {
+        'cbar': False,
+        'linewidths': 0.2,
+        'linecolor': 'white',
+        'annot': True}
+    
+    #Start creating plotly plot
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(
+        go.Bar(x=df.date, y=df.error,
+               # text=round(metrics.R2, 2),
+               marker_color='dodgerblue', opacity=0.7,
+               name="Error"),
+        secondary_y=False
+    )
+    
+    fig.add_trace(
+        go.Scatter(x=df.date, y=df.next_day_rain_mm,
+                   mode='markers', name="Next day rain"),
+        secondary_y=True
+    )
+    
+    fig.add_trace(
+        go.Scatter(x=df.date, y=df.rain_amount_mm_prediction,
+                   mode='markers', name="Rain prediction"),
+        secondary_y=True
+    )
+    
+    
+    fig.update_traces(marker=dict(size=5.5,
+                                  line=dict(width=0.3, color='black')),
+                      selector=dict(mode='markers'))
+    fig.update_layout(
+        title="Detailed predictions, actuals and errors for selected station",
+        xaxis_title="Date",
+        yaxis_title="Rain amount",
+        legend_title="Legend",
+    )
+    fig.update_layout(xaxis_type='category')
+    fig.update_xaxes(tickangle=270)
+    fig.update_yaxes(range=[y_min,y_max], secondary_y=False)
+    fig.update_yaxes(range=[y_min,y_max],title_text="Rain amount", secondary_y=True)
+    st.plotly_chart(fig)
